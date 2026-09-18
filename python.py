14c3f0ec20d58e3ef9b1cbfbc9de7ce3b4114f6b
charankotthapally-sys <charankotthapally@gmail.com>
2026-09-18 14:27:08 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,92 @@
+#printing message
+print('Hello World!.')
+print('Welcome to Python.')
+print('SriCharan')
+print('i am learning python')
+print('python programming')
+print('good morning')
+print('welcome students')
+print('python is easy')
+print('my first program')
+print('hello from python')
+
+#quotes,messages nd calc
+print('2 + 5')
+print(2+5)
+print('10 - 3')
+print(10-3)
+print('hello world!')
+print('python')
+print('hello')
+print('hello')
+print('10 + 20')
+print(10+20)
+
+#variables 
+name='Sri charan'
+age=20
+marks=85
+price=99.5
+city='chennai'
+print(city)
+score=95
+print(score)
+x=10
+print(x)
+message='welcome'
+print(message)
+a=10
+b=20
+print(a+b)
+quantity=5
+price=10
+print(quantity*price)
+
+#data types
+print(type(25))
+print(type(25.5))
+print(type('25'))
+age=21
+height=5.8
+name='arun' 
+age=20
+price=99.5
+name='ravi'
+marks=90
+print(type(marks))
+temp=36.5
+print(type(temp))
+message='name'
+print(type(name))
+
+#sequences
+print('start')
+print('middle')
+print('end')
+x=10
+print(x)
+x=20
+print(x)
+number=5
+print(number)
+number=10
+print(number)
+print('A,B,C')
+name='ravi'
+age=20
+print(name)
+print(age)
+print(1)
+print(2)
+print(3)
+print(4)
+x=5
+y=10
+print(x)
+print(y)
+print('start')
+print(2+3)
+print('end')
+print('login')
+print('dashboard')
+print('logout')
